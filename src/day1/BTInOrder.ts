8591ee24ca7d433c01b84e472dcf05a2c18b3728
victor <victorwrong96@gmail.com>
2024-05-20 12:02:29 -0400

    tree traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,17 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // recurse
+    walk(curr.left, path);
+    path.push(curr.value);
+    walk(curr.right, path);
+
+    // post
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
