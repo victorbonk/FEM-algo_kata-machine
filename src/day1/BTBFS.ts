2a724fd4ef38fa92ac02ca1e499092e2effcb9dd
Victor Hong <victor@Victors-Mac-mini.local>
2024-05-27 11:21:00 -0400

    comments

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,7 +1,9 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    // uses a queue (FIFO)
     const q: (BinaryNode<number> | null)[] = [head];
 
     while (q.length) {
+        // First out
         const curr = q.shift() as BinaryNode<number> | undefined | null;
         if (!curr) {
             continue;
@@ -12,7 +14,9 @@ export default function bfs(head: BinaryNode<number>, needle: number): boolean {
             return true;
         }
 
+        // left in
         q.push(curr.left);
+        // right in
         q.push(curr.right);
     }
 
